31d69adc10bdb60cfafa8d4eacbbfed71be9e0b6
Umbasa <butuzov-tema@yandex.ru>
2023-11-29 09:34:48 +0300

    ++Math/ 989. Add to Array-Form of Integer

---
+++ LeetCode/Math/989. Add to Array-Form of Integer.cpp
@@ -0,0 +1,41 @@
+﻿
+using namespace std;
+#include<vector>
+
+class Solution
+{
+
+public:
+    vector<int> addToArrayForm(vector<int> num, int k)
+    {
+        for (auto it = num.rbegin(); it != num.rend(); ++it)
+        {
+            auto v = k % 10;
+            if (*it + v > 9)
+            {
+                *it = (*it + v) % 10;
+                k /= 10;
+                ++k;
+            }
+            else
+            {
+                k /= 10;
+                *it += v;
+            }
+        }
+        if (k > 0) {
+            while (k != 0)
+            {
+                auto v = k % 10;
+                num.insert(num.begin(), v);
+                k /= 10;
+            }
+        }
+        return num;
+    }
+};
+int main() {
+    Solution a;
+    a.addToArrayForm({ 9 }, 111);
+    //a.addToArrayForm({ 2,1,5 }, 806);    
+}
