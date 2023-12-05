4535c24df5ccc8a9e694bba4c926cdf9fd13c71f
Umbasa <butuzov-tema@yandex.ru>
2023-12-05 07:46:30 +0300

    ++string/ 2278. Percentage of Letter in String

---
+++ LeetCode/String/2278. Percentage of Letter in String.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include<algorithm>
+
+
+//383. Ransom Note
+
+using namespace std;
+
+class Solution {
+public:
+    int percentageLetter(string s, char letter)
+    {
+        int result = count(s.begin(), s.end(), letter);
+        result = 100.0 / s.size() * result;
+        return result;
+    }
+};
+
+
+int main()
+{
+    //This is a sample sample text. This text contains contains a sample.
+    setlocale(LC_ALL, "Russian");
+    string rhs = "foobar";
+    char lhs = 'o';
+    auto result = a.percentageLetter(rhs, lhs);
+    return 0;
+}
